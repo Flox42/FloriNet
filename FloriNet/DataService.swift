58a7c1ee8d1be6fd5a9279f6476848edce006b6c
Florian Poncelin <Flori@Florians-MacBook-Air.local>
2016-08-25 17:00:11 +0200

    Installed Firebase Auth and Database pods, implemented email login and user creation in database

---
+++ FloriNet/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  FloriNet
+//
+//  Created by Florian Poncelin on 22/08/2016.
+//  Copyright © 2016 Florian Poncelin. All rights reserved.
+//
+
+import Foundation
---
+++ FloriNet/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  FloriNet
+//
+//  Created by Florian Poncelin on 24/08/2016.
+//  Copyright © 2016 Florian Poncelin. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
